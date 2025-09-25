ff3546a5c85b6b062675a22a2ac9622eb859a5e9
Azizbek Asadov <azizbek1998@icloud.com>
2025-09-26 00:26:15 +0200

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Azizbek Asadov on 26.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
